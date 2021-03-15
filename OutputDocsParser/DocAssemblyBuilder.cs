92176419766d59982294427dae2dd46521da5fb8
Angourisoft <lev.brovchenkov@gmail.com>
2021-03-15 19:11:39 +0300

    upload

---
+++ OutputDocsParser/DocAssemblyBuilder.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OutputDocsParser
+{
+    public static class NamespaceParser
+    {
+        public static string OneFoldBack(string name)
+        {
+            if (name.Contains('('))
+                name = name.Substring(0, name.IndexOf('('));
+            return name.Substring(0, name.LastIndexOf('.'));
+        }
+
+        public static string LastFold(string name)
+        {
+            string withNoPss;
+            if (name.Contains('('))
+                withNoPss = name.Substring(0, name.IndexOf('('));
+            else
+                withNoPss = name;
+            return name[(withNoPss.LastIndexOf('.') + 1)..];
+        }
+
+        private static (string name, string pars) SplitMethodInto(string methodSignature)
+        {
+            string name;
+            string pars;
+
+            if (methodSignature.Contains('`'))
+                name = methodSignature.Substring(0, methodSignature.IndexOf('`'));
+            else if (methodSignature.Contains('('))
+                name = methodSignature.Substring(0, methodSignature.IndexOf('('));
+            else
+                name = methodSignature;
+
+            if (methodSignature.Contains('('))
+                pars = methodSignature.Substring(methodSignature.IndexOf('('));
+            else
+                pars = "";
+
+            return (name, pars);
+        }
+
+        public static string GetMethodName(string methodSignature)
+            => SplitMethodInto(methodSignature).name;
+
+        public static string GetMethodParams(string methodSignature)
+            => SplitMethodInto(methodSignature).pars;
+    }
+
+    public sealed class DocAssemblyBuilder
+    {
+        public string? Name { set; get; }
+
+        public readonly SortedDictionary<string, DocNamespaceBuilder> Namespaces = new();
+
+        public DocAssembly Build()
+            => new DocAssembly(Name ?? throw new NullReferenceException(), 
+                Namespaces.Select(c => c.Value.Build()).ToArray());
+
+        public DocNamespaceBuilder InsertNamespace(string name)
+            => Namespaces.TryGetValue(name, out var res) ? res : Namespaces[name] = new() { Name=name };
+
+        public DocAssemblyBuilder Parse(Dictionary<string, string> members)
+        {
+            foreach (var pair in members)
+            {
+                var techName = pair.Key;
+                var text = pair.Value;
+                if (techName.StartsWith("T:") && techName[2..] is var fullClassName)
+                {
+                    var namespaceName = NamespaceParser.OneFoldBack(fullClassName);
+                    var className = NamespaceParser.LastFold(fullClassName);
+                    InsertNamespace(namespaceName)
+                        .InsertClass(className).Description = text;
+                }
+                else
+                if (techName.StartsWith("P:") && techName[2..] is var fullPropertyName)
+                {
+                    fullClassName = NamespaceParser.OneFoldBack(fullPropertyName);
+                    var namespaceName = NamespaceParser.OneFoldBack(fullClassName);
+                    var className = NamespaceParser.LastFold(fullClassName);
+                    var propertyName = NamespaceParser.LastFold(fullPropertyName);
+                    InsertNamespace(namespaceName)
+                        .InsertClass(className)
+                            .InsertProperty(propertyName)
+                                .Description = text;
+                }
+                else
+                if (techName.StartsWith("M:") && techName[2..] is var fullMethodName)
+                {
+                    fullClassName = NamespaceParser.OneFoldBack(fullMethodName);
+                    var namespaceName = NamespaceParser.OneFoldBack(fullClassName);
+                    var className = NamespaceParser.LastFold(fullClassName);
+                    var methodSignature = NamespaceParser.LastFold(fullMethodName);
+                    var methodName = NamespaceParser.GetMethodName(methodSignature);
+                    var methodParams = NamespaceParser.GetMethodParams(methodSignature);
+                    InsertNamespace(namespaceName)
+                        .InsertClass(className)
+                            .InsertMethod(methodName)
+                                .InsertOverload(methodParams, text);
+                }
+            }
+
+            return this;
+        }
+    }
+
+    public sealed record DocAssembly(string Name, IEnumerable<DocNamespace> Namespaces);
+
+    public sealed class DocNamespaceBuilder
+    {
+        public DocClassBuilder InsertClass(string name)
+            => Classes.TryGetValue(name, out var res) ? res : Classes[name] = new() { Name=name };
+
+        public string? Name { get; set; }
+
+        public SortedDictionary<string, DocClassBuilder> Classes { get; } = new();
+
+        public DocNamespace Build() => new DocNamespace(Name ?? throw new NullReferenceException(), Classes.Select(c => c.Value.Build()).ToArray());
+    }
+
+    public sealed record DocNamespace(string Name, IEnumerable<DocClass> Classes);
+
+    public sealed class DocClassBuilder
+    {
+        public string? Name { get; set; }
+
+        public string Description { get; set; } = "";
+
+        public SortedDictionary<string, DocMemberBuilder> Members { get; } = new();
+
+        public DocClass Build()
+            => new DocClass(Name ?? throw new NullReferenceException(), Description ?? throw new NullReferenceException(), Members.Values.Select(c => c.Build()).ToArray());
+
+        public DocPropertyBuilder InsertProperty(string name)
+        {
+            if (Members.TryGetValue(name, out var res))
+                return (DocPropertyBuilder)res;
+            var prop = new DocPropertyBuilder();
+            prop.Name = name;
+            Members[name] = prop;
+            return prop;
+        }
+
+        public DocMethodBuilder InsertMethod(string name)
+        {
+            if (Members.TryGetValue(name, out var res))
+                return (DocMethodBuilder)res;
+            var method = new DocMethodBuilder();
+            method.Name = name;
+            Members[name] = method;
+            return method;
+        }
+    }
+
+    public sealed record DocClass(string Name, string Description, IEnumerable<DocMember> Members);
+
+    public abstract class DocMemberBuilder
+    {
+        public abstract DocMember Build();
+    }
+
+    public sealed class DocMethodBuilder : DocMemberBuilder
+    {
+        public string? Name { get; set; }
+        public SortedDictionary<string, DocOverload> Overloads { get; } = new();
+        public override DocMethod Build() => new DocMethod(Overloads.Values, Name ?? throw new NullReferenceException());
+        public DocOverload InsertOverload(string pars, string description)
+            => Overloads.TryGetValue(pars, out var res) ? res : Overloads[pars] = new DocOverload(pars, description);
+    }
+
+    public sealed class DocPropertyBuilder : DocMemberBuilder
+    {
+        public string? Name { get; set; }
+        public string Description { get; set; } = "";
+        public override DocProperty Build() => new DocProperty(Name ?? throw new NullReferenceException(), Description ?? throw new NullReferenceException());
+    }
+
+    public abstract record DocMember;
+
+    public sealed record DocMethod(IEnumerable<DocOverload> Overloads, string Name) : DocMember;
+
+    public sealed record DocOverload(string Parameters, string Description);
+
+    public sealed record DocProperty(string Name, string Description) : DocMember;
+
+
+
+    public static class DocsParser
+    {
+        public static DocAssemblyBuilder Parse(string path)
+        {
+            var settings = new XmlReaderSettings
+            {
+                IgnoreWhitespace = true
+            };
+            using var fileStream = File.OpenText(path);
+            using var reader = XmlReader.Create(fileStream, settings);
+
+            while (reader.Read() && reader.Name != "members") { }
+
+            var members = new Dictionary<string, string>();
+
+            reader.Read();
+
+            while (reader.Name != "members")
+            {
+                if (reader.EOF)
+                    break;
+                var attrValue = reader.GetAttribute("name");
+                var innerXml = reader.ReadInnerXml();
+                members[attrValue ?? throw new NullReferenceException()] = innerXml;
+                if (reader.Name != "member")
+                {
+                    while (reader.Read() && reader.Name != "member") ;
+                    reader.Read();
+                }
+            }
+
+            return new DocAssemblyBuilder().Parse(members);
+        }
+    }
+}
---
+++ OutputDocsParser/WebsiteBuilder.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputDocsParser
+{
+    public interface IPageSave
+    {
+        public void Save(string path, string text);
+    }
+
+    public sealed class WebsiteBuilder
+    {
+        private readonly IPageSave saver;
+        public WebsiteBuilder(IPageSave saver)
+            => this.saver = saver;
+
+        public void Build(DocAssembly assembly)
+        {
+            saver.Save("namespaces.html", BuildNamespaceList(assembly));
+            foreach (var nspace in assembly.Namespaces)
+                saver.Save(nspace.Name + ".html", BuildClassList(nspace));
+
+            foreach (var nspace in assembly.Namespaces)
+                foreach (var cls in nspace.Classes)
+                    saver.Save($"{nspace.Name}/{cls.Name}.html", BuildMemberList(cls, nspace.Name + ".html"));
+        }
+
+        private static string BuildNamespaceList(DocAssembly assembly)
+            =>
+            h2_centered(assembly.Name) +
+
+            hr() +
+
+            p("Here we describe all the namespaces and outer classes within the assembly") +
+
+            p(
+                ul(
+                    assembly.Namespaces.Select(
+                        nspace => a(nspace.Name + ".html", nspace.Name)
+                        )
+                )
+            );
+
+        private static string BuildClassList(DocNamespace nspace)
+            =>
+            h2_centered(nspace.Name) +
+
+            hr() +
+
+            p(a("namespaces.html", "&#8592; Back to list of namespaces")) +
+
+            p($"Classes within the {nspace.Name} namespace") +
+
+            p(
+                ul(
+                    nspace.Classes.Select(
+                        cls
+                            => 
+                                (cls.Members.Count() > 0
+                                ?
+                                a($"{nspace.Name}/{cls.Name}.html", cls.Name)
+                                :
+                                cls.Name
+                                ) + 
+                                DescriptionFromXml(cls.Description)
+                    )
+                )
+            );
+
+        private static string BuildMemberList(DocClass cls, string back)
+            =>
+            h2_centered(cls.Name) +
+
+            hr() +
+
+            p(a($"../{back}", "&#8592; Back to list of classes")) +
+
+            h3("Description") +
+
+            DescriptionFromXml(cls.Description) +
+
+            h3("Members") +
+
+            p(
+                ul(
+                    cls.Members.Select(
+                        member
+                            => member switch
+                            {
+                                DocMethod method =>
+                                    method.Overloads.Count() == 1 
+
+                                    ?
+
+                                    p(
+                                        $"Method {method.Name + method.Overloads.First().Parameters}"
+                                    ) +
+                                    p(DescriptionFromXml(method.Overloads.First().Description))
+
+                                    :
+
+                                    p(
+                                        $"Method {method.Name} and its overloads"
+                                    ) +
+
+                                    ul(
+                                        method.Overloads.Select(
+                                            overload =>
+                                                p($"{method.Name}{overload.Parameters}") +
+                                                p(DescriptionFromXml(overload.Description))
+                                        )
+                                    )
+
+                                ,
+                                DocProperty property =>
+                                    p(
+                                        $"Property {property.Name}"
+                                    ) +
+                                    p(DescriptionFromXml(property.Description)),
+                                _ => throw new Exception()
+                            }
+                    )
+                )
+            );
+
+        private static string tag(string text, string tag)
+            => $"<{tag}>{text}</{tag}>";
+
+        private static string tagClass(string text, string tag, string @class)
+            => $"<{tag} class=\"{@class}\">{text}</{tag}>";
+
+        private static string p(string text)
+            => tag(text, "p");
+        private static string ul(IEnumerable<string> points)
+            => tag(string.Join("", points.Select(c => li(c))), "ul");
+        private static string li(string text)
+            => tag(text, "li");
+        private static string h1(string text)
+            => tag(text, "h1");
+        private static string h2(string text)
+            => tag(text, "h2");
+        private static string h2_centered(string text)
+            => tagClass(text, "h2", "centered");
+        private static string h3(string text)
+            => tag(text, "h3");
+        private static string hr() => "<hr/>";
+        private static string a(string href, string text)
+            => $"<a href=\"{href}\">{text}</a>";
+
+
+        private static string FixLinks(string xml)
+        {
+            if (!xml.Contains("<a"))
+                return xml;
+            var id = xml.IndexOf("<a");
+            var nextId = xml.IndexOf("/>", id);
+            if (nextId == -1)
+                return xml;
+            var linkInside = xml.Substring(id + 2, nextId - id - 2);
+            if (linkInside.Contains('<') || linkInside.Contains('>'))
+                return xml;
+            return xml.Substring(0, nextId) + ">Link</a>" + FixLinks(xml.Substring(nextId + 2));
+        }
+        private static string DescriptionFromXml(string xml)
+        {
+            xml = FixLinks(xml);
+            return xml;
+        }
+    }
+}
