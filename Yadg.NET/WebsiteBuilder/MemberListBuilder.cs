30d5684f8fb929fee99f18f8e7860c26ecc963cc
WhiteBlackGoose <wbg@angouri.org>
2022-07-16 08:48:51 +0300

    minor

--- Yadg.NET/WebsiteBuilder/MemberListBuilder.cs
+++ Yadg.NET/WebsiteBuilder/MemberListBuilder.cs
@@ -43,34 +43,23 @@ namespace YadgNet
 
                                     p(
                                         a($"./{cls.Name}/{method.Name}.html", h3(SplitWrapWordsMethod(method.Name + method.Overloads.First().Parameters))) + "Method"
-                                    ) +
-                                    p(new DescriptionFromXmlBuilder(method.Overloads.First().Description, "../").RemoveTag("example").Build())
+                                    )
 
                                     :
 
                                     p(
-                                        a($"./{cls.Name}/{method.Name}.html", h3(method.Name)) + " Method and its overloads"
-                                    ) /*+
-
-                                    ul("yadg-list-2",
-                                        method.Overloads.Select(
-                                            overload =>
-                                                h3(SplitWrapWordsMethod(method.Name + overload.Parameters)) +
-                                                p(new DescriptionFromXmlBuilder(overload.Description, "../").RemoveTag("example").Build())
-                                        )
-                                    )*/
+                                        a($"./{cls.Name}/{method.Name}.html", h3(method.Name)) + "Method and its overloads"
+                                    )
                                     )
                                 ,
                                 DocProperty property =>
                                     p(
-                                        a($"./{cls.Name}/{property.Name}.html", h3(property.Name)) + " Property"
-                                    ) +
-                                    p(new DescriptionFromXmlBuilder(property.Description, "../").RemoveTag("example").Build()),
+                                        a($"./{cls.Name}/{property.Name}.html", h3(property.Name)) + "Property"
+                                    ),
                                 DocField field =>
                                     p(
-                                        a($"./{cls.Name}/{field.Name}.html", h3(field.Name)) + " Field"
-                                    ) +
-                                    p(new DescriptionFromXmlBuilder(field.Description, "../").RemoveTag("example").Build()),
+                                        a($"./{cls.Name}/{field.Name}.html", h3(field.Name)) + "Field"
+                                    ),
                                 _ => throw new Exception()
                             }
                     )
